c5279fefa25339d48fd4919d5ce082bea7423160
Jason Flair <emelieobumse100@gmail.com>
2023-02-12 12:10:14 +0100

    recommendations work now

--- web_flask/submit_recommend.py
+++ web_flask/submit_recommend.py
@@ -1,8 +1,73 @@
 #!/usr/bin/python3
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+def submit_song():
     """gets the song details from user and works using the details"""
-    song_name, artist = input("Enter song name and title")
+    song_name = input("Enter song name: ").replace(".", "")
+    artist = input("Enter artist name: ")
+    print()
+    headers = {
+        "Authorization": "Bearer BQCEGwHr7B8ISalWrK27Al-Z_pOAyTNN7bDkX8mBRLr66sQXXWeErceeSpxzBBZiGRHhI54AWH5TlW27oXdMRMPYDVhI34I8MhtRrVJpLIUqgLiKViQj1U3rBeB1Cd_G9yAqTNzrNTS4MaHAcpmYA5g6aAR9wOrIh-gZ-8Batrc4Bcl90-SkBfQ1c7XIvhzAVppC"}
+    search_song_url = f"https://api.spotify.com/v1/search?q={song_name}+ artist:{artist}&type=track"
+    response = requests.get(search_song_url, headers=headers)
+    json_resp = response.json()
+    track_id = json_resp['tracks']['items'][0]['id']
+    artist_id = json_resp['tracks']['items'][0]['album']['artists'][0]['id']
+    """print(track_id)
+    print()
+    print(artist_id)
+    print()"""
+    search_features_url = f"https://api.spotify.com/v1/audio-features/{track_id}"
+    features_response = requests.get(search_features_url, headers=headers)
+    features_json = features_response.json()
+    danceability = features_json['danceability']
+    tempo = features_json['tempo']
+    key = features_json['key']
+
+    """print(f"here are the audio features of {song_name}:")
+    print(f"danceability: {danceability}")
+    print(f"liveness: {liveness}")
+    print(f"tempo: {tempo}")
+    print(f"key: {key}")"""
+
+    get_recommendations(danceability, tempo, key, track_id, artist_id, song_name)
+
+def get_recommendations(danceability, tempo, key, track_id, artist_id, song_name):
+
+    """gets recommendations based on parameters passed"""
+    headers = { "Authorization": "Bearer BQCEGwHr7B8ISalWrK27Al-Z_pOAyTNN7bDkX8mBRLr66sQXXWeErceeSpxzBBZiGRHhI54AWH5TlW27oXdMRMPYDVhI34I8MhtRrVJpLIUqgLiKViQj1U3rBeB1Cd_G9yAqTNzrNTS4MaHAcpmYA5g6aAR9wOrIh-gZ-8Batrc4Bcl90-SkBfQ1c7XIvhzAVppC"}
+
+    min_danceability = danceability - 0.031
+    max_danceability = danceability + 0.3
+    min_tempo = tempo - 4
+    max_tempo = tempo + 10
+    min_key = key - 1
+    max_key = key + 3
+    min_popularity = 15
+    max_popularity = 100
+
+    recommendations_url = f"https://api.spotify.com/v1/recommendations?limit=10&seed_artists={artist_id}&seed_tracks={track_id}" \
+                          f"&min_danceability={min_danceability}&max_danceability={max_danceability}&min_key={min_key}" \
+                          f"&max_key={max_key}&min_popularity={min_popularity}&max_popularity={max_popularity}&min_tempo={min_tempo}&max_tempo={max_tempo}"
+
+
+    recommendations_resp = requests.get(recommendations_url, headers=headers)
+    recommendations_json = recommendations_resp.json()
+
+    print(f"based on your favourite song, {song_name}, here are some songs we think you might like:")
+    print()
+    for track in recommendations_json['tracks']:
+        artist = track['artists'][0]['name']
+        track_title = track['name']
+        track_title = track_title.replace(".", "")
+        if track_title.lower() != song_name.lower():
+            print(f"{track_title} by {artist}")
+
+
+
+
+if __name__ == "__main__":
+    submit_song()
--- web_static/test.py
+++ web_static/test.py
@@ -4,6 +4,97 @@ if __name__ == '__main__':
     url = f"https://api.spotify.com/v1/search?q={'arcadia'}+ artist:{'lana del rey'}&type=track"
     response = requests.get(url, headers=headers)
     json_resp = response.json()
+    print(json_resp)
     track_id = json_resp['tracks']['items'][0]['id']
     print(track_id)
+
+from flask import Flask
+import requests
+import pandas as pd
+from sklearn.mixture import GaussianMixture
+
+app = Flask(__name__)
+
+
+
+import pandas as pd
+import numpy as np
+from sklearn.mixture import GaussianMixture
+
+# Load the audio features data into a pandas DataFrame
+df = pd.read_csv("audio_features.csv")
+
+# Preprocess the data (if necessary)
+# ...
+
+# Train the GaussianMixture model on the audio features
+X = df[["danceability", "liveness", "tempo", "key"]].values
+gmm = GaussianMixture(n_components=10)
+gmm.fit(X)
+
+# Find the audio features of the input song
+input_song = np.array([[0.721, 0.341, 124.02, 7]])
+
+# Calculate the likelihood of the input song under each Gaussian distribution
+probs = gmm.predict_proba(input_song)
+
+# Find the indices of the songs with the highest probabilities
+indices = np.argmax(probs, axis=1)
+
+# Select the top k songs with the highest probabilities
+k = 5
+top_k = df.iloc[indices][:k]
+
+# Print the top k songs
+print("Top %d similar songs:" % k)
+for i, row in top_k.iterrows():
+    print("- Song: %s" % row["song_id"])
+    print("  Danceability: %.3f" % row["danceability"])
+    print("  Liveness: %.3f" % row["liveness"])
+    print("  Tempo: %.2f" % row["tempo"])
+    print("  Key: %d" % row["key"])
+
+
+def submit_song():
+    """gets the song details from user and works using the details"""
+    song_name = input("Enter song name: ")
+    artist = input("Enter artist name: ")
+    headers = {
+        "Authorization": "Bearer BQDW5fiDeEC1jEPhaZgDdBJtgKd-8t63Nv1mZ6tsggeiPra0q"
+                         "rRXhiGbBj2S7uL73ToiynkwZL_lX96xlQDEdzdtUVPB5e0Q40S58siTGFNd7K4o4iw3KeimWVxq"
+                         "jwpE1QIw3givzx7xXzKIypgkxUPCgN7V_W4x1jeKknOLRPw"
+                         "drM9mYbUSuS1UfMKV8OQsCttx"}
+    search_song_url = f"https://api.spotify.com/v1/search?q={song_name}+ artist:{artist}&type=track"
+    response = requests.get(search_song_url, headers=headers)
+    json_resp = response.json()
+    track_id = json_resp['tracks']['items'][0]['id']
+    search_features_url = f"https://api.spotify.com/v1/audio-features/{track_id}"
+    features_response = requests.get(search_features_url, headers=headers)
+    features_json = features_response.json()
+    danceability = features_json['danceability']
+    tempo = features_json['tempo']
+    liveness = features_json['liveness']
+    key = features_json['key']
+
+    submitted_song_features = [danceability, liveness, tempo, key]
+
+    # Load the dataset of audio features
+    dataset = pd.read_csv("audio_features.csv")
+
+    # Select the columns you want to use for building the GaussianMixture model
+    X = dataset[['danceability', 'liveness', 'tempo', 'key']]
+
+    # Train the GaussianMixture model
+    gmm = GaussianMixture(n_components=10)
+    gmm.fit(X)
+
+    # Use the GaussianMixture model to predict the similarity of the submitted song to the songs in the dataset
+    similarity_scores = gmm.score_samples([submitted_song_features])
+
+    # Get the indices of the top 5 most similar songs
+    top_5_indices = similarity_scores.argsort()[-5:][::-1]
+
+    # Print the details of the top 5 most similar songs
+    print
+
 
