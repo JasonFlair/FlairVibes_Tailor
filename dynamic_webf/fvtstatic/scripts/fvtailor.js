92766ee04dd14a66b238793879a1830f81ddf180
Jason Flair <emelieobumse100@gmail.com>
2023-03-02 15:29:26 +0100

    some more cleanups

--- dynamic_webf/fvtstatic/scripts/fvtailor.js
+++ dynamic_webf/fvtstatic/scripts/fvtailor.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    // direct the user to the flair vibes tailor from the landing page whenever any of these buttons is clicked.
+    $('#tryit_button').click(function() {
+        window.location = 'http://' + location.hostname + '/fvt';
+    });
+    $('#cta_button').click(function() {
+        window.location = 'http://' + location.hostname + '/fvt';
+    });
+
+
+    // code for the flair vibes tailor itself
     const apiUrl = 'http://' + location.hostname + ':5006/submit_song';
     let query_data = {}
     $('form#submit_songs').submit(function(event) {
@@ -18,17 +28,21 @@ $(document).ready(function(){
                 $('section.welcome').empty();
                 $('section.submission').empty();
                 $('section.recommendations').empty();
+
+                // get tracks from the submit_song endpoint reponse
                 let tracks = response['tracks'];
-                console.log(tracks)
                 let thankYouNote = `<h2 class="thankyounote">Thank you for using Flair Vibes Tailor</h2>`
                 let message = `<h4 class="recommendation-text">Based on your favourite song, <b>${songTitle}</b>, here are some songs we think you might like!</h4>`;
                 let goagainButton = `<form>
                     <input type="submit" value="ouu i wanna go again!" id="goagainButton">
                 </form>`
+
+                // append the thank you note and the message.
                 $('section.welcome').append(thankYouNote);
                 $('section.recommendations').append(message);
                 for (track of tracks) {
                     parsedTrack = JSON.parse(track)
+                    // each track returned by the response is listed/rendered to the screen
                     let recommendations = `
                     <ul class="recommendation-list">
                     <li><b>${parsedTrack['title']}</b> by ${parsedTrack['artist']}
@@ -39,6 +53,8 @@ $(document).ready(function(){
                 }
                 $('section.recommendations').append(goagainButton);
             },
+
+            // handle errors 400 and 500
             error: function(tokenErr, textStatus, errorThrown) {
                 $('section.recommendations').empty();
                 if (tokenErr.status === 400) {
@@ -46,6 +62,7 @@ $(document).ready(function(){
                 let message = `<h4>oops something went wrong, please try again some other time</h4>`;
                 $('section.recommendations').append(message);
                 } else if (tokenErr.status === 500) {
+                    // if the error returned is 500, then it is most likely that there is an error in the song details submitted
                     let message = `<h4>oops something went wrong, please check your submitted details</h4>
                                     <p>note: the song submited might not be present on major streaming services like apple music and/or spotify</p>`;
                     $('section.recommendations').append(message);
@@ -53,14 +70,10 @@ $(document).ready(function(){
             }
         });
     });
+
+    // reloads the page if the go again button is clicked
     $('form#goagainButton').submit(function(event) {
         event.preventDefault();
         location.reload(true);
-    });
-    $('#tryit_button').click(function() {
-        window.location = 'http://' + location.hostname + '/fvt';
-    });
-    $('#cta_button').click(function() {
-        window.location = 'http://' + location.hostname + '/fvt';
     });
 });
